f8d3d1f73c9d2422359e7e20717b001bc19fe722
felixHerder <felixbaciu@gmail.com>
2021-06-15 12:45:56 +0300

    added Hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, { name: "Bob", animal: "Dog", breed: "Labrador" }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Chino",
-  //     animal: "Cat",
-  //     breed: "Shorthair",
-  //   }),
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Bob" animal="Dog" breed="Labrador" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Chino" animal="Cart" breed="Shorthair" />
+      <SearchParams />
     </div>
   )
 };
